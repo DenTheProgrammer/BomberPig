6fed84ec2d267f52bcf2cc5cac36d6e392cb1580
Denis Mozhayskiy <den4ikk007@gmail.com>
2022-01-11 18:10:57 +0300

    Bombs for all fix

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,16 +7,18 @@ public class Player : ADamageableObject
 {
     [SerializeField]
     private GameObject bombPrefab;
-    
+    private PhotonView view;
 
     private void Start()
     {
+        view = GetComponent<PhotonView>();
         BombButton.onBombButtonPress += PlaceBomb;
     }
 
     public void PlaceBomb()
     {
-        PhotonNetwork.Instantiate(bombPrefab.name, gameObject.transform.position + new Vector3(0f, -0.1f), Quaternion.identity);
+        if (view.IsMine)
+            PhotonNetwork.Instantiate(bombPrefab.name, gameObject.transform.position + new Vector3(0f, -0.1f), Quaternion.identity);
         //Instantiate(bombPrefab, gameObject.transform.position + new Vector3(0f, -0.1f), Quaternion.identity);
     }
 
